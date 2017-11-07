ff2f191b32e87db984488a664f91045e91ba6194
Joe Fogg <GJF610@gmail.com>
2017-11-06 22:16:31 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+
+
+
+
+
+
+
+
+-- USE `burgers_db`;
+
+-- -- insert 3 burgers
+-- 	INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Wheres The Beef', false, CURRENT_TIMESTAMP);
+-- 	INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Cheezeburger Cheezeburgerf', false, CURRENT_TIMESTAMP);
+-- 	INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Big Mamas Wonder Bread Burger', false, CURRENT_TIMESTAMP);
